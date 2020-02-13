df46db27e11437eed2f57772c5a342d9e6a4c12e
nickdpz <ndpastranzamora@gmail.com>
2020-02-13 01:32:02 -0300

    Nueva vista

--- limpo/app.js
+++ limpo/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require("./routes/login");//crea la variable y la vincula con el archivo
 
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use("/login", loginRouter);//Crea la ruta
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
