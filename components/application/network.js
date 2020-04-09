41157285f7df9614bffeff65f66c842626626433
FranLopezP <francisco-9708@hotmail.es>
2020-04-09 00:01:43 -0500

    add Node

--- components/application/network.js
+++ components/application/network.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const passport = require('passport')
 const Session = require('../../network/auth');
-const router = express.Router();// Igual a la función Roputer para separar por cabeceras metodos de petición
+const router = express.Router(); // Igual a la función Roputer para separar por cabeceras metodos de petición
 const controller = require('./controller');
-const response = require('../../network/response')//Trae network dos carpetas arriba
+const response = require('../../network/response') //Trae network dos carpetas arriba
 
 
 router.get('/', Session.isAuthenticated, (req, res) => {
@@ -11,53 +11,59 @@ router.get('/', Session.isAuthenticated, (req, res) => {
 })
 
 router.post('/api', Session.isAuthenticated, (req, res) => {
-    const data = req.body.message;
-    if(data==="data"){
-        const info = [{
-            "type": "LineString",
-                "coordinates": [[4.62805, -74.06556], [4.633429, -74.067715]]
+        const data = req.body.message;
+        if (data === "data") {
+            const info = [{
+                "type": "LineString",
+                "coordinates": [
+                    [4.62805, -74.06556],
+                    [4.633429, -74.067715]
+                ]
             }, {
                 "type": "LineString",
-                "coordinates": [[4.633429, -74.0677152], [4.630399, -74.067818]]
+                "coordinates": [
+                    [4.633429, -74.0677152],
+                    [4.630399, -74.067818]
+                ]
             }];
-        response.success(req, res, info, 201)
-    }
-})
-//----- New Node-------------
-router.get('/newNode', Session.isAuthenticated,  (req, res) =>{
-    //const customersDB = await Customer.find();
-    res.render('application/addNode');//,{customersDB}
-})
-//----- Add Node --------------------Session.isAuthenticated,
-router.post('/node',(req,res)=>{
-    const { eui, model} = req.body; //Destructuring 
-    controller.addNode(eui, model)
-        .then((info)=>{
             response.success(req, res, info, 201)
-        })
-        .catch((e)=>{
-            response.error(req, res, 'Información Invalida',300,e)
-        })  
-})
-//----- Obtener Nodos ------------------Session.isAuthenticated,
-router.get('/node/',  (req, res) => {
-    const filter = req.query.eui || null;
-    controller.getNodes(filter)
-        .then((info)=>{
-            response.success(req, res, info, 201)
-        })
-        .catch((e)=>{
-            response.error(req, res, 'Información Invalida',300,e)
-        }) 
-})
-//----- Obtener Nodos ------------------Session.isAuthenticated,
-router.delete('/node/:eui',  (req, res) => {
+        }
+    })
+    //----- New Node-------------
+router.get('/newNode', Session.isAuthenticated, (req, res) => {
+        //const customersDB = await Customer.find();
+        res.render('application/addNode'); //,{customersDB}
+    })
+    //----- Add Node --------------------Session.isAuthenticated,
+router.post('/node', (req, res) => {
+        const { eui, model } = req.body; //Destructuring 
+        controller.addNode(eui, model)
+            .then((info) => {
+                response.success(req, res, info, 201)
+            })
+            .catch((e) => {
+                response.error(req, res, 'Información Invalida', 300, e)
+            })
+    })
+    //----- Obtener Nodos ------------------Session.isAuthenticated,
+router.get('/node', (req, res) => {
+        const eui = req.query.eui || null;
+        controller.getNodes(eui)
+            .then((info) => {
+                response.success(req, res, info, 200)
+            })
+            .catch((e) => {
+                response.error(req, res, 'Información Invalida', 300, e)
+            })
+    })
+    //----- Obtener Nodos ------------------Session.isAuthenticated,
+router.delete('/node/:eui', (req, res) => {
     controller.deleteNode(req.params.eui)
-        .then((info)=>{
+        .then((info) => {
             response.success(req, res, info, 201)
         })
-        .catch((e)=>{
-            response.error(req, res, 'Información Invalida',300,e)
-        }) 
+        .catch((e) => {
+            response.error(req, res, 'Información Invalida', 300, e)
+        })
 })
 module.exports = router;
---
+++ public/javascripts/app/addNode.js
@@ -0,0 +1,53 @@
+const eui = document.getElementById('eui');
+const model = document.getElementById('model');
+const btnSubmit = document.getElementById('btnSubmit');
+
+btnSubmit.addEventListener("click", async(event) => {
+    if (eui) {
+        await fetch("/app/node", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    eui: eui.value,
+                    model: model.value
+                }),
+            })
+            .then(response => response.json()).then((data) => {
+                if (!data.error) {
+                    alert("Registro Exitoso");
+                } else {
+                    alert(data.error);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error)
+            });
+    } else {
+        alert('Faltan Campos');
+    }
+})
+
+
+
+eui.addEventListener("blur", async(event) => {
+    if (eui) {
+        await fetch(`/app/node?eui=${eui.value}`, {
+                method: "GET"
+            })
+            .then(response => response.json()).then((data) => {
+                const aux = data.message[0]
+                if (!aux) {
+                    eui.className = 'form-control';
+                } else {
+                    eui.className = 'form-control is-invalid';
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error)
+            });
+    } else {
+        alert('Faltan Campos');
+    }
+})
