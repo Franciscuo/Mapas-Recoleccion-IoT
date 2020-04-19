4dfa20fe04338db38ace32cfd7afcd4d9a4a3f9b
nickdpz <ndpastranzamora@gmail.com>
2020-04-18 19:31:52 -0500

    Nuevo manera de manejar errores util para desarrollo

--- network/response.js
+++ network/response.js
@@ -1,18 +1,24 @@
-//exporta esta funcion para ser usada en otro archivo
+
+const withErrorStack=(error, message)=>{
+    if (process.env.NODE_ENV==='developed') {
+        console.log(error)
+        return { 'error':error,
+                'message':message
+             }
+    }
+    return { 'error':message}
+}
+
 const success = (req, res, messages, status) => { 
     console.log(status)
-    res.status(status || 200).send({//si esta indefined status, responde 200
+    res.status(status || 200).send({
         'error':'',
         'message':messages
     })
 }
 
-const error = (req, res, error, status, message)=>{ //exporta esta funcion para ser usada en otro archivo
-    console.error(`[response error] ${message}`)
-    res.status(status || 500).send({//si esta indefined status, responde 200
-        'error':error,
-        'message':''
-    })
+const error = (req, res, message, status, error)=>{
+    res.status(status || 500).send(withErrorStack(error.errmsg, message));
 }
 
 module.exports = {success, error};
