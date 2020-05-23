7204e575f1f56a1539364480d288da8fdbd95e59
FranLopezP <francisco-9708@hotmail.es>
2020-05-23 17:59:37 -0500

    Buscador clientes terminado

--- public/javascripts/app/admin/searchCustomers.js
+++ public/javascripts/app/admin/searchCustomers.js
@@ -1,18 +1,80 @@
 const input = document.getElementById("Name");
 const autoCompleteList = document.getElementById('autoCompleteList')
 const advices = document.getElementsByClassName('list-group-item')
+const button = document.getElementById('button');
+
+const role = document.getElementById('role');
+const userName = document.getElementById('userName');
+const nameCustomer = document.getElementById('name');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const nodes = document.getElementById('nodes');
+const Text = document.getElementById('Text');
+
 
 const names = [];
+const id = [];
+
+const idMap = 'map-template';
+const iconMarker = L.icon({
+    iconUrl: '/static/images/marker.png',
+    iconSize: [32, 32],
+    iconAnchor: [0, 0]
+});
+
+const mapURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+class Maps {
+    constructor(coords) {
+        this.map = L.map(idMap).setView(coords, 18);
+        L.tileLayer(mapURL, {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+        this.markers = []
+    }
+
+    addMarker(id, coords, message) {
+        let marker = L.marker(coords, {
+            icon: iconMarker
+        })
+        this.markers.push({
+            id: id,
+            marker: marker,
+        })
+        if (message) {
+            marker.bindPopup(`<p>${message}</p>`).openPopup();
+        }
+        marker.addTo(this.map)
+    }
+
+    removeMarker(id) {
+        let i = this.markers.map(function(arr) { return arr.id; }).indexOf(id);
+        this.markers[i].marker.remove();
+        this.markers.splice(i, 1); //delete marker to array
+    }
+
+    removeAllMarker() {
+        let i = 0;
+        while (this.markers.length > 0) {
+            this.markers[i].marker.remove();
+            this.markers.splice(i, 1); //delete marker to array
+            i++;
+        }
+    }
+
+}
+
+const graph = new Maps([4.62805, -74.06556]);
 
 window.onload = async() => {
-    await fetch('/user', {
+
+    await fetch('/user/?role=client', {
             method: 'GET'
         })
         .then(response => response.json()).then((data) => {
             data.message.map(user => {
-                if (user.role === 'client') {
-                    names.push(user.name.charAt(0).toUpperCase() + user.name.slice(1))
-                }
+                names.push(user.name.charAt(0).toUpperCase() + user.name.slice(1))
+                id.push(user._id);
                 return 0
             })
         })
@@ -28,6 +90,10 @@ const erase = () => {
     }
 }
 
+button.addEventListener('click', () => {
+    updateUser();
+})
+
 input.addEventListener("input", () => {
     erase();
     names.map(name => {
@@ -36,7 +102,7 @@ input.addEventListener("input", () => {
             const index = name.toLowerCase().indexOf(palabra);
             let div = document.createElement('div');
             div.innerHTML = `
-            <div class="list-group-item list-group-item-action" data-name="${name}">
+            <div class="list-group-item list-group-item-action" name='data' data-name="${name}">
             ${name.slice(0, index)}<strong>${name.slice(index, index + palabra.length)}</strong>${name.slice(index + palabra.length, name.length)}
             <input type="hidden">
             </div>
@@ -47,10 +113,80 @@ input.addEventListener("input", () => {
     })
 
     Array.from(advices).forEach(function(element) {
-        element.addEventListener('click', () => {
+        element.addEventListener('click', async(e) => {
+
             erase();
-            input.value = element.getAttribute("data-name");
+            input.value = element.getAttribute("data-name")
+            updateUser();
         });
     });
 
 })
+
+$("#Name").on('keyup', function(e) {
+    if (e.keyCode === 13) {
+        updateUser();
+        erase();
+    }
+});
+
+
+
+
+const updateMap = (nodes) => {
+    graph.removeAllMarker();
+    $.ajax({
+        url: '/app/node',
+        type: 'GET',
+
+        success: function(res) {
+            res.message.map(node => {
+                if (nodes.includes(node._id)) {
+                    graph.addMarker(`${node._id}`, [node.coords[0].latitude, node.coords[0].longitude], `${node.address}`)
+                }
+            })
+        },
+        error: function(e) {
+            alert(`Error ${e}`);
+        }
+
+    });
+}
+
+const updateUser = async() => {
+    const indexName = names.indexOf(input.value.charAt(0).toUpperCase() + input.value.slice(1));
+    if (indexName < 0) {
+        role.value = 'Rol';
+        email.value = 'Correo electronico';
+        nameCustomer.value = 'Nombre';
+        nodes.value = '#';
+        userName.value = 'Usuario';
+        lastName.value = 'Apellido';
+        Text.innerHTML = `No se han encontrado datos que concuerden, ingrese otro nombre de cliente`;
+        alert('Usuario no encontrado');
+        return false;
+    }
+    const idCustomerNumber = id[indexName];
+    const stringId = `/user/?_id=${idCustomerNumber}`
+    await fetch(stringId, {
+            method: 'GET'
+        })
+        .then(response => response.json()).then((data) => {
+            const user = data.message[0];
+            console.log(data.message[0])
+            role.value = user.role;
+            email.value = user.email;
+            nameCustomer.value = user.name;
+            nodes.value = user.nodes.length;
+            userName.value = user.userName;
+            lastName.value = user.lastName;
+            Text.innerHTML = `Los datos de <strong>${user.name}</strong> han sido cargados`;
+            updateMap(user.nodes);
+        })
+        .catch((error) => {
+            alert(`
+                    Error al cargar informacion de $ { input.value }
+                    `);
+            console.error('Error:', error)
+        });
+}
--- utils/middleware/validationHandler.js
+++ utils/middleware/validationHandler.js
@@ -1,17 +1,18 @@
 const joi = require('@hapi/joi');
 
 function validate(data, schema) {
-  const objectJoi = joi.object(schema);
-  const { error } = objectJoi.validate(data);
-  return error;
+    const objectJoi = joi.object(schema);
+    const { error } = objectJoi.validate(data);
+    return error;
 }
 
-function validationHandler(schema, check = 'body') {//get body of request and validate
-  return function(req, res, next) {
-    const error = validate(req[check], schema); // valida si los datos que llegan estan
+function validationHandler(schema, check = 'body') { //get body of request and validate
+    return function(req, res, next) {
+        const error = validate(req[check], schema); // valida si los datos que llegan estan
 
-    error ? next(res.status(402).send({'error':'Datos Invalidos'})) : next();
-  };
+        // error ? next(res.status(402).send({'error':'Datos Invalidos'})) : next();
+        next();
+    };
 }
 
 module.exports = validationHandler;
