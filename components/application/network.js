2f780a4fd93f5c0440d8a39075b16f1045c6e975
nickdpz <ndpastranzamora@gmail.com>
2020-05-24 17:05:08 -0500

    Vistas para diferentes tipos de usuario

--- components/application/network.js
+++ components/application/network.js
@@ -42,22 +42,48 @@ router.delete('/node/:eui', (req, res) => {
 // principal
 router.get('/', Session.isAuthenticated, (req, res) => {
     if (req.user.role === "admin") {
-        res.render('application/admin/ctlAdmin.hbs');
+        res.render('application/admin/ctlAdmin.hbs',{
+            user: req.user,
+            name: user.name,
+        });
     } else if (req.user.role === "client") {
-        res.render('application/client/ctlClient.hbs');
+        res.render('application/client/ctlClient.hbs',{
+            user: req.user,
+            name: req.user.name,
+        });
     } else if (req.user.role === "worker") {
-        res.render('application/worker/ctlWorker.hbs');
-    } else {
-        res.render('application/normal/ctlNormal.hbs');
+        res.render('application/worker/ctlWorker.hbs',{
+            user: req.user,
+            name: req.user.name,
+        });
+    } else {//req.user.role === "none"
+        res.render('application/none/ctlNone.hbs',{
+            user: req.user,
+            name: req.user.name,
+        });
     }
 })
 
 // --- Redireciones Admin
 //----- New Node-------------
 router.get('/viewNewNode', Session.isAuthenticated, (req, res) => {
-    //const customersDB = await Customer.find();
-    res.render('application/admin/viewAddNode.hbs'); //,{customersDB}
+    if (req.user.role === "admin") {
+        res.render('application/admin/viewAddNode.hbs'); //,{customersDB}
+    }else if(req.user.role === "client"){
+        res.render('application/client/viewAddNode.hbs'); //,{customersDB}
+    }else if(req.user.role === "none"){
+        res.render('application/none/viewAddNode.hbs'); //,{customersDB}
+    }
 })
+
+//------- Vista de Rutas
+router.get('/viewRoutes', Session.isAuthenticated, (req, res) => {
+    if (req.user.role === "admin") {
+        res.render('application/admin/viewRoutes.hbs')
+    }else if(req.user.role === "client"){
+        res.render('application/client/viewRoutes.hbs')
+    }
+});
 
 // ----  View Zones -----------------
 router.get('/viewZones/:zone', Session.isAuthenticated, (req, res) => {
@@ -65,12 +91,7 @@ router.get('/viewZones/:zone', Session.isAuthenticated, (req, res) => {
     const name = controller.nameZone(zone);
     res.render('application/admin/zone.hbs', { zone, name })
 });
-
-//------- Vista de Rutas
-router.get('/viewRoutes', Session.isAuthenticated, (req, res) => {
-    res.render('application/admin/viewRoutes.hbs')
-});
-
+// ----- View Config Routes ----
 router.get('/configRoutes', Session.isAuthenticated, (req, res) => {
     res.render('application/admin/configRoutes.hbs')
 })
@@ -79,31 +100,12 @@ router.get('/configRoutes', Session.isAuthenticated, (req, res) => {
 router.get('/viewClients', Session.isAuthenticated, (req, res) => {
         //const customersDB = await Customer.find();
         res.render('application/admin/viewClients.hbs'); //,{customersDB}
-    })
+})
+
     // ---- Vista Workers
 router.get('/viewWorkers', Session.isAuthenticated, (req, res) => {
     //const customersDB = await Customer.find();
     res.render('application/admin/viewWorkers.hbs'); //,{customersDB}
 })
-
-//----- Obtener rutas ------------------Session.isAuthenticated,
-router.get('/routes', Session.isAuthenticated, (req, res) => {
-    res.render('admin/routes.hbs')
-});
-
-//----- Añadir rutas ------------------Session.isAuthenticated,
-router.post('/routesMap', (req, res) => {
-    const zone = 16;
-    const nodes = ["5e8e9076a7bee74bf0e1e7e3", "5e8eaac857065825342402f3"];
-    controller.addRoute(zone, nodes)
-        .then((info) => {
-            response.success(req, res, info, 200)
-        })
-        .catch((e) => {
-            response.error(req, res, 'Información Invalida', 300, e)
-        })
-})
-
-
 
 module.exports = router;
