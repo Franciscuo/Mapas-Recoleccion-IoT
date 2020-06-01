856d70cc94a53e7ae2fd113c6cb7ca261bf2a563
nickdpz <ndpastranzamora@gmail.com>
2020-06-01 16:58:28 -0500

    Carga de los datos correcta

--- public/javascripts/app/admin/configRoutes.js
+++ public/javascripts/app/admin/configRoutes.js
@@ -1,21 +1,41 @@
 const startLatField = document.getElementById('startLat');
 const startLonField = document.getElementById('startLon');
-const startLatField = document.getElementById('startLat');
+const centerLonField = document.getElementById('centerLon');
+const centerLatField = document.getElementById('centerLat');
 const endLonField = document.getElementById('endLon');
 const endLatField = document.getElementById('endLat');
 const zoneField = document.getElementById('zone');
+const capacityField = document.getElementById('capacity');
+const btnEdit = document.getElementById('btnEdit');
+const change = false;
 
+
+const changeInputs = async () => {
+    if(!change) {
+        btnEdit.disabled = false;
+        change = true;
+    }
+}
 
 const enabledInput = ()=>{
     startLonField.disabled = false;
     startLatField.disabled = false;
     endLonField.disabled = false;
-    endLonField.disabled = false;
-    //capacity.disabled = false;
+    endLatField.disabled = false;
+    centerLonField.disabled = false;
+    centerLatField.disabled = false;
+    capacityField.disabled = false;
+    startLatField.addEventListener("change",changeInputs);
+    startLonField.addEventListener("change",changeInputs);
+    centerLatField.addEventListener("change",changeInputs);
+    endLonField.addEventListener("change",changeInputs);
+    centerLatField.addEventListener("change",changeInputs);
+    centerLonField.addEventListener("change",changeInputs);
+    capacityField.addEventListener("change",changeInputs);
 }
 
 const getZone = async() => {
-    await fetch(`/zone/?number=${zoneField.value}`, {
+    await fetch(`/zones/?number=${zoneField.value}`, {
         method: 'GET',
     })
     .then(response => response.json()).then((data) => {
@@ -23,7 +43,13 @@ const getZone = async() => {
             console.log('Error:',data.error)
             alert("A ocurrido un problema intente mas tarde")
         }else{
-
+            startLatField.value = data.message[0].start[0];
+            startLonField.value = data.message[0].start[1];
+            endLatField.value = data.message[0].end[0];
+            endLonField.value = data.message[0].end[1];
+            centerLatField.value = data.message[0].center[0];
+            centerLonField.value = data.message[0].center[1];
+            capacityField.value = data.message[0].capacity;
         }
     })
     .catch((error) => {
@@ -32,18 +58,11 @@ const getZone = async() => {
 }
 
 zoneField.addEventListener("change", async(event) => {
-
+    await getZone();
+    enabledInput();
 })
 
-startLonField.addEventListener("blur", async(event) => {
-    alert("efectivo")
+btnEdit.addEventListener("click", async(event) => {
+    alert("Hola");
 })
-endLonField.addEventListener("blur", async(event) => {
-    alert("efectivo")
-})
-endLatField.addEventListener("blur", async(event) => {
-    alert("efectivo")
-})
-endLonField.addEventListener("blur", async(event) => {
-    alert("efectivo")
-})
+
